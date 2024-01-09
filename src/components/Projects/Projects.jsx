7e2906b486719f7520896cdd46c9aa167d7535f0
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2024-01-09 22:45:23 +0000

    Restructure projects
    If applied, commit will:
    - Remove chat, flix and meet components and instead implement pop up modals for each project card to display information

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,11 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
-import { Button, Card, Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-import { Chat } from "./Chat";
-import { Meet } from "./Meet";
-import { Flix } from "./Flix";
+//Separately importing due to Typeerror bug
+import Card  from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 import ChatImg from "../../img/Chat.png";
 import MeetImg from "../../img/Meet.png";
@@ -59,61 +60,102 @@ const StyledDiv = styled.div`
     .projects {
         font-family: Silkscreen;
     }
+`
 
-    #backbutton {
-        color: white;
-        border-color: white;
+const StyledModal = styled(Modal)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledModalContent = styled.div`
+    
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    max-width: 600px;
+    width: 100%;
+    max-height: 600px;
+    justify-content: center;
+    text-align: center;
+
+    h1 {
+        font-family: Black Ops One;
+    }
+
+    p {
+        font-family: Silkscreen;
     }
 `
 
 export const Projects = () => {
     const [selectedProject, setSelectedProject] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const projectInfo = {
+        Chat: {
+          title: "Chat App",
+          description: "'Chat' the mobile application created with React Native, Firebase and Expo was designed to allow users to receive and send messages with anonymous authentication. Messages within conversations can be displayed whilst users are offline, and users would be also be able to send photos and their own location.",
+          link: "https://github.com/WorldAdrastea/Chat-App"
+        },
+        Meet: {
+          title: "Meet App",
+          description: "An application created with React that allows users to find events that they can filter by location and technology. This project uses AWS Lambda, Google OAuth, Recharts, Jest, Cucumber and Puppeteer. The application itself uses the Google Calendar API to fetch upcoming events.",
+          link: "https://github.com/WorldAdrastea/meetv2"
+        },
+        myFlix: {
+          title: "myFlix App",
+          description: "'myFlix' is a web application project built with the MERN tech stack. It was made to allow users to create an account to log into a database of movies. Users would have information about each individual movie and be able to save a list of their favourite movies. They would be able to update their account information and delete it if necessary.",
+          link: "https://github.com/WorldAdrastea/myFlix-Client"
+        },
+    };
 
     const handleNavItemClick = (projectName) => {
         setSelectedProject(projectName);
+        setIsModalOpen(true);
     };
 
     const handleGoBack = () => {
         setSelectedProject(null);
+        setIsModalOpen(false);
     };
+
     return (
         <StyledDiv>
-            {selectedProject === null && (
-                <Typography variant="h1">
-                    Select a project:
-                </Typography>
-            )}
-            {selectedProject === null && (
-                <div className="cards">
-                    <Card onClick={() => handleNavItemClick("Chat")} data-app-name="Chat App" >
-                        <img src={ChatImg} />
-                    </Card>
-                    <Card onClick={() => handleNavItemClick("Meet")} data-app-name="Meet App" >
-                        <img src={MeetImg} />
-                    </Card>
-                    <Card onClick={() => handleNavItemClick("myFlix")} data-app-name="myFlix App" >
-                        <img src={myFlixImg} />
-                    </Card>
-                </div>
-            )}
-            {selectedProject !== null && (
-                <div>
-                    <Button id="backbutton" variant="outlined" onClick={handleGoBack}>
-                        Back
-                    </Button>
-                </div>
-            )}
-            <div onProjectClick={handleNavItemClick}>
-                <section className="projects" id="Chat" style={{ display: selectedProject === "Chat" ? "block" : "none" }}>
-                    <Chat />
-                </section>
-                <section className="projects" id="Meet" style={{ display: selectedProject === "Meet" ? "block" : "none" }}>
-                    <Meet />
-                </section>
-                <section className="projects" id="myFlix" style={{ display: selectedProject === "myFlix" ? "block" : "none" }}>
-                    <Flix />
-                </section>
+            <Typography variant="h1">
+                Select a project:
+            </Typography>
+            <div className="cards">
+                <Card onClick={() => handleNavItemClick("Chat")} data-app-name="Chat App" >
+                    <img src={ChatImg} />
+                </Card>
+                <Card onClick={() => handleNavItemClick("Meet")} data-app-name="Meet App" >
+                    <img src={MeetImg} />
+                </Card>
+                <Card onClick={() => handleNavItemClick("myFlix")} data-app-name="myFlix App" >
+                    <img src={myFlixImg} />
+                </Card>
             </div>
+            <StyledModal 
+                open={isModalOpen} 
+                onClose={handleGoBack}
+            >
+                <StyledModalContent>
+                    {selectedProject !== null && (
+                        <div >
+                            <Typography variant="h1">{projectInfo[selectedProject].title}</Typography>
+                            <Typography variant="body1">{projectInfo[selectedProject].description}</Typography>
+                            <a
+                                href={projectInfo[selectedProject].link}
+                                target="_blank" 
+                            >
+                                <GitHubIcon className="icons" fontSize="large" />
+                            </a>
+                        </div>
+                    )}
+                    {selectedProject === null && <p>No project selected</p>}
+                </StyledModalContent>
+            </StyledModal>
         </StyledDiv>
     );
 };
